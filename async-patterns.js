91efb3f7c28c56f97bcf468e2d4c6638525e46f5
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-09 19:09:07 +0530

    Async patterns with promises and await

---
+++ async-patterns.js
@@ -0,0 +1,28 @@
+// promises
+
+const { readFile, writeFile } = require('fs')
+
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, 'utf-8', (err, data) => {
+      if (err) reject(err)
+
+      resolve(data)
+    })
+  })
+}
+
+// getText('./content/first.txt')
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err))
+
+const start = async () => {
+  try {
+    const first = await getText('./content/first.txt')
+    console.log(first)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
