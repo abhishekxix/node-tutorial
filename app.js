bf4b32868a76cbf6d17b1cce24fdc3b3e8051b80
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-09 17:29:15 +0530

    modules

---
+++ app.js
@@ -0,0 +1,7 @@
+// Modules
+
+sayHi('Abhishek');
+
+const { a, b, c } = require('./names');
+console.log(a, b, c);
+sayHi = require('./names');
---
+++ names.js
@@ -0,0 +1,10 @@
+const a = 'abhishek';
+const b = 'aldag';
+const c = 'ahakldfhkja';
+const sayHi = (name) => {
+  console.log(`hello there ${name}`);
+};
+
+// module.exports = { a, b, c };
+
+module.exports = sayHi;
